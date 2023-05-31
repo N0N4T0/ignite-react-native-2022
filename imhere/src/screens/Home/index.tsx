ded73848e45d6258e50b9949f4a49bd41424543e
Lucio <luciononato@hotmail.com>
2023-05-31 19:51:19 -0300

    feat: create component Participant

--- imhere/src/screens/Home/index.tsx
+++ imhere/src/screens/Home/index.tsx
@@ -1,5 +1,6 @@
 import { View, Text, TextInput, TouchableOpacity } from 'react-native'
 import { styles } from './styles'
+import { Participant } from '../../components/Participant';
 
 export function Home(){
   function handleParticipantAdd() {
@@ -29,6 +30,10 @@ export function Home(){
           </Text>
         </TouchableOpacity>
       </View>
+
+      <Participant/>
+      <Participant/>
+      <Participant/>
     </View>
   )
 }
--- imhere/src/screens/Home/styles.ts
+++ imhere/src/screens/Home/styles.ts
@@ -23,7 +23,8 @@ export const styles = StyleSheet.create({
     borderRadius: 5,
     color: '#fff',
     padding: 16,
-    fontSize: 16
+    fontSize: 16,
+    flex: 1
   },
   buttonText: {
     color: '#FFF',
