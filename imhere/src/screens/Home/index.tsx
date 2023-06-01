21fb9097b1bf8ec0cc0c6c035e0eb2fb5f11787f
Lucio <luciononato@hotmail.com>
2023-05-31 21:00:28 -0300

    fix: add Flatlist

--- imhere/src/screens/Home/index.tsx
+++ imhere/src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, TouchableOpacity, ScrollView } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, FlatList } from 'react-native'
 import { styles } from './styles'
 import { Participant } from '../../components/Participant';
 
@@ -38,17 +38,23 @@ export function Home(){
         </TouchableOpacity>
       </View>
 
-      <ScrollView showsVerticalScrollIndicator={false}>
-        {
-          participants.map(participant => (
-            <Participant 
-              key={participant} 
-              name={participant} 
-              onRemove={() => handleParticipantRemove(participant)} 
-            />
-          ))
-        }
-      </ScrollView>
+      <FlatList 
+        data={participants}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Participant 
+            key={item} 
+            name={item} 
+            onRemove={() => handleParticipantRemove("Rodrigo")} 
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>
+            Ninguém chegou no evento ainda? Adicione participantes a sua lista de presença.
+          </Text>
+        )}
+      />
       
     </View>
   )
