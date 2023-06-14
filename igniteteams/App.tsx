50e155f4257a77ecd5e6502d2357919b663719cc
Lucio <luciononato@hotmail.com>
2023-06-13 23:01:49 -0300

    fix: add expo fonts

--- igniteteams/App.tsx
+++ igniteteams/App.tsx
@@ -1,12 +1,18 @@
-import { Groups } from "@screens/Groups";
-import theme from "./src/theme";
-import { ThemeProvider } from "styled-components";
+import { ActivityIndicator } from 'react-native';
+import { ThemeProvider } from 'styled-components/native'; 
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import theme from './src/theme';
+
+import { Groups } from '@screens/Groups';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-
-   <Groups/>
+      <Groups/>
+      { fontsLoaded ? <Groups/> : <ActivityIndicator /> }
     </ThemeProvider>
   );
 }
