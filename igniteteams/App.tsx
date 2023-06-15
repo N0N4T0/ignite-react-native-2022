7b84b7b3f06400607006301c62c11c4a15ff9786
Lucio <luciononato@hotmail.com>
2023-06-14 21:25:22 -0300

    fix: add Loading component

--- igniteteams/App.tsx
+++ igniteteams/App.tsx
@@ -1,10 +1,10 @@
-import { ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components/native'; 
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import theme from './src/theme';
 
 import { Groups } from '@screens/Groups';
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <Groups/>
-      { fontsLoaded ? <Groups/> : <ActivityIndicator /> }
+      { fontsLoaded ? <Groups/> : <Loading /> }
     </ThemeProvider>
   );
 }
