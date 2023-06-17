2c39cf0777e5793aded0ab53e42e9c863014c36c
Lucio <luciononato@hotmail.com>
2023-06-17 15:05:23 -0300

    fix: add BackButton

--- igniteteams/src/components/Header/index.tsx
+++ igniteteams/src/components/Header/index.tsx
@@ -1,10 +1,20 @@
-import { Container, Logo } from "./styles";
+import { BackButton, BackIcon, Container, Logo } from "./styles";
 
 import logoImg from '@assets/logo.png';
+import { HeaderProps } from "./types";
 
-export function Header() {
+export function Header(props: HeaderProps) {
+  const {showBackButton = false} = props
+
   return (
     <Container>
+      { 
+        showBackButton &&
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      }
+      
       <Logo source={logoImg} />
     </Container>
   )
