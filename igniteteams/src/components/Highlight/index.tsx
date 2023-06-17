79d6886b32ea34dfb32884c0755ac542cd876797
Lucio <luciononato@hotmail.com>
2023-06-17 15:31:56 -0300

    fix: add Highlight component

---
+++ igniteteams/src/components/Highlight/index.tsx
@@ -0,0 +1,18 @@
+import { Container, Subtitle, Title } from "./styles";
+import { HighlightProps } from "./types";
+
+export function Highlight(props: HighlightProps) {
+const {title, subtitle} = props
+
+  return (
+    <Container>
+      <Title>
+        {title}
+      </Title>
+
+      <Subtitle>
+        {subtitle}
+      </Subtitle>
+    </Container>
+  )
+}
