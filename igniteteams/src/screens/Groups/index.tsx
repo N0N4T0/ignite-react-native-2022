a1c0905a2d8c647b92142b5965ff73f4e4c0c147
Lucio <luciononato@hotmail.com>
2023-06-23 18:36:00 -0300

    feat: create Button component

--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -4,7 +4,8 @@ import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 import { useState } from 'react';
 import { FlatList } from 'react-native';
-import { ListEmpty } from "@components/ListEmpty";
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
@@ -29,6 +30,10 @@ export function Groups() {
         ListEmptyComponent={() => (
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
+      />
+
+      <Button 
+        title='Criar nova turma'
       />
     </Container>
   );
