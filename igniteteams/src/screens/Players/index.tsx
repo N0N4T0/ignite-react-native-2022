406c22b10b98f4b526b6d90507de2d50fd796d5c
Lucio <luciononato@hotmail.com>
2023-06-25 16:13:58 -0300

    feat: create component Filter

--- igniteteams/src/screens/Players/index.tsx
+++ igniteteams/src/screens/Players/index.tsx
@@ -4,6 +4,7 @@ import { Highlight } from "@components/Highlight";
 import { Container, Form } from "./styles";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
+import { Filter } from "@components/Filter";
 
 export function Players() {
   return (
@@ -25,6 +26,11 @@ export function Players() {
           icon="add" 
         />
       </Form>
+
+      <Filter
+        title="Time A"
+        isActive
+      />
     </Container>
   )
 }
