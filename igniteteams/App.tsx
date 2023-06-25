89f6f83649611584349b01d95431204bc52bf2d1
Lucio <luciononato@hotmail.com>
2023-06-25 10:20:13 -0300

    feat create PLayer component

--- igniteteams/App.tsx
+++ igniteteams/App.tsx
@@ -7,6 +7,7 @@ import { Groups } from '@screens/Groups';
 import { Loading } from '@components/Loading';
 import { StatusBar } from 'react-native';
 import { NewGroup } from '@screens/NewGroup';
+import { Players } from '@screens/Players';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -14,7 +15,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar barStyle='light-content' backgroundColor='transparent' translucent/>
-      { fontsLoaded ? <NewGroup/> : <Loading /> }
+      { fontsLoaded ? <Players/> : <Loading /> }
     </ThemeProvider>
   );
 }
