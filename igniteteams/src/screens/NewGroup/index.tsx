8bb82b13cc3a86baba043af8631e99d758efa1b9
Lucio <luciononato@hotmail.com>
2023-07-13 22:15:39 -0300

    fix: add useFocusEffect

--- igniteteams/src/screens/NewGroup/index.tsx
+++ igniteteams/src/screens/NewGroup/index.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 
-import { Header } from "@components/Header";
+import { groupCreate } from "@storage/group/groupCreate";
+
 import { Container, Content, Icon } from "./styles";
+
+import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
@@ -12,8 +15,13 @@ export function NewGroup(){
 
   const navigation = useNavigation()
 
-  function handleNew(){
-    navigation.navigate('players', { group })
+  async function handleNew() {
+    try {
+      await groupCreate(group);
+      navigation.navigate('players', { group })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
