ee8522af2c52781f2a4a62a8c4eb6af7d2c921d1
Lucio <luciononato@hotmail.com>
2023-08-22 20:58:04 -0300

    refactor: button component

--- ignitegym/src/components/Button.tsx
+++ ignitegym/src/components/Button.tsx
@@ -1,30 +1,15 @@
-import { Button as ButtonNativeBase, IButtonProps, Text } from 'native-base';
+import React from "react";
+
+import { Button as NativeBaseButton, IButtonProps } from "native-base";
 
 type Props = IButtonProps & {
   title: string;
-}
+};
 
-export function Button({ title, variant,...rest }: Props) {
+export function Button({ title, ...rest }: Props) {
   return (
-    <ButtonNativeBase
-      w="full"
-      h={14}
-      bg={variant === 'outline' ? 'transparent' : 'green.700'}
-      borderWidth={variant === 'outline' ? 1 : 0}
-      borderColor="green.500"
-      rounded="sm"
-      _pressed={{
-        bg: variant === 'outline' ? 'gray.500' : 'green.500'  
-      }}
-      {...rest}
-    >
-      <Text 
-        color={variant === 'outline' ? 'green.500' : 'white'}
-        fontFamily="heading"
-        fontSize="sm"
-      >
-        {title}
-      </Text>
-    </ButtonNativeBase>
+    <NativeBaseButton mb={4} {...rest}>
+      {title}
+    </NativeBaseButton>
   );
 }
--- ignitegym/src/theme/index.ts
+++ ignitegym/src/theme/index.ts
@@ -1,28 +1,28 @@
-import { extendTheme } from 'native-base';
+import { extendTheme } from "native-base";
 
 export const THEME = extendTheme({
   colors: {
     green: {
-      700: '#00875F',
-      500: '#00B37E',
+      700: "#00875F",
+      500: "#00B37E",
     },
     gray: {
-      700: '#121214',
-      600: '#202024',
-      500: '#29292E',
-      400: '#323238',
-      300: '#7C7C8A',
-      200: '#C4C4CC',
-      100: '#E1E1E6'
+      700: "#121214",
+      600: "#202024",
+      500: "#29292E",
+      400: "#323238",
+      300: "#7C7C8A",
+      200: "#C4C4CC",
+      100: "#E1E1E6",
     },
-    white: '#FFFFFF',
+    white: "#FFFFFF",
     red: {
-      500: '#F75A68'
-    }
+      500: "#F75A68",
+    },
   },
   fonts: {
-    heading: 'Roboto_700Bold',
-    body: 'Roboto_400Regular',
+    heading: "Roboto_700Bold",
+    body: "Roboto_400Regular",
   },
   fontSizes: {
     xs: 12,
@@ -33,6 +33,41 @@ export const THEME = extendTheme({
   },
   sizes: {
     14: 56,
-    33: 148
-  }
-})
+    33: 148,
+  },
+  components: {
+    Button: {
+      defaultProps: {
+        h: 14,
+        w: "full",
+        rounded: "sm",
+      },
+      variants: {
+        solid: {
+          bg: "green.700",
+          _pressed: {
+            bg: "green.500",
+          },
+          _text: {
+            fontSize: "md",
+            fontFamily: "heading",
+          },
+        },
+        outline: {
+          borderColor: `gray.100`,
+          borderWidth: 1,
+          bg: "transparent",
+          rounded: "full",
+          _pressed: {
+            bg: "gray.500",
+          },
+          _text: {
+            color: "green.500",
+            fontSize: "md",
+            fontFamily: "heading",
+          },
+        },
+      },
+    },
+  },
+});
