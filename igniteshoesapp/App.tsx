d9f37c4daed8b9f58d42d961a1d1b66f55aaa83f
Lúcio Nonato Silva de Oliveira <n0n4t0@MacBook-Air-de-Lucio.local>
2024-10-12 13:58:35 -0300

    fix: change android build number and add onesiginal permission

--- igniteshoesapp/App.tsx
+++ igniteshoesapp/App.tsx
@@ -11,6 +11,7 @@ import { CartContextProvider } from './src/contexts/CartContext';
 import { OneSignal } from 'react-native-onesignal';
 
 OneSignal.initialize("547f21ad-5ea8-4311-83f9-8bbba1098d11")
+OneSignal.Notifications.requestPermission(true)
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
