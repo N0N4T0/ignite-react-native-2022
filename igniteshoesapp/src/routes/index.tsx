b04dce07885f4556c453c76bac1b7ea9da960222
Lúcio Nonato Silva de Oliveira <n0n4t0@MacBook-Air-de-Lucio.local>
2024-10-13 18:02:32 -0300

    fix: add deep linking configuration

--- igniteshoesapp/src/routes/index.tsx
+++ igniteshoesapp/src/routes/index.tsx
@@ -6,6 +6,20 @@ import { NotificationWillDisplayEvent, OneSignal, OSNotification } from 'react-n
 import { AppRoutes } from './app.routes';
 import { Notification } from '../components/Notification';
 
+const linking = {
+  prefixes: ["com.rocketseat.igniteshoes://", "igniteshoes://"],
+  config: {
+    screens: {
+      details: {
+        path: "/details/:productId",
+        parse: {
+          productId: (productId: string) => productId,
+        }
+      }
+    }
+  }
+}
+
 export function Routes() {
   const { colors } = useTheme();
   const [notification, setNotification] = useState<OSNotification>()
@@ -33,7 +47,7 @@ export function Routes() {
   }, [])
 
   return (
-    <NavigationContainer theme={theme}>
+    <NavigationContainer theme={theme} linking={linking}>
       <AppRoutes />
 
       {
