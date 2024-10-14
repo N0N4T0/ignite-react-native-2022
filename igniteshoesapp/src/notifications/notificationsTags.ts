f7acaa653c984fe4f67bb7dad471ff241f63feb3
Lúcio Nonato Silva de Oliveira <n0n4t0@MacBook-Air-de-Lucio.local>
2024-10-13 22:02:14 -0300

    fix: removeTag and add tag to abandonned cart

--- igniteshoesapp/src/notifications/notificationsTags.ts
+++ igniteshoesapp/src/notifications/notificationsTags.ts
@@ -1,8 +1,13 @@
 import { OneSignal } from "react-native-onesignal";
 
 export function tagUserInfoCreate(){
-    OneSignal.User.addTags({
-        user_name: "Android",
-        user_email: "android@gmail.com",
-    })
+    // OneSignal.User.addTags({
+    //     user_name: "Android",
+    //     user_email: "android@gmail.com",
+    // })
+    OneSignal.User.removeTag("user_email")
+}
+
+export function tagCartUpdate(itemsCount: string){
+    OneSignal.User.addTag("cart_items_count", itemsCount)
 }
