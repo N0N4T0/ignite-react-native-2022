12d9afa0f07ee2b480f4abee478754b4418bba39
Lúcio Nonato Oliveira <lucio.nonato@kivid.com.br>
2024-11-17 22:31:22 -0300

    fix: customize default theme

--- ignitegymv2/App.tsx
+++ ignitegymv2/App.tsx
@@ -1,23 +1,21 @@
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
-import { StatusBar, Text, View } from 'react-native';
+import { config } from './config/gluestack-ui.config';
+import { Center, GluestackUIStyledProvider, Text } from '@gluestack-ui/themed';
+import { StatusBar, View } from 'react-native';
 
 export default function App() {
   const [fonstLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
-    <View style={{
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: '#202024'
-    }}
-    >
+    <GluestackUIStyledProvider config={config}>
+
       <StatusBar
         barStyle='light-content'
         backgroundColor="transparent"
         translucent
       />
-      {fonstLoaded ? <Text>Home</Text> : <View />}
-    </View>
+      {fonstLoaded ? <Center flex={1} bg='$gray700'><Text>Home</Text></Center> : <View />}
+
+    </GluestackUIStyledProvider>
   );
 }
