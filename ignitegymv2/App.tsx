c54d54d3988c7cc39fb19a452d86f8708eae9a00
Lúcio Nonato Oliveira <lucio.nonato@kivid.com.br>
2024-11-20 22:41:32 -0300

    feat: create Loading component

--- ignitegymv2/App.tsx
+++ ignitegymv2/App.tsx
@@ -1,7 +1,8 @@
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 import { config } from './config/gluestack-ui.config';
 import { Center, GluestackUIStyledProvider, Text } from '@gluestack-ui/themed';
-import { StatusBar, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fonstLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -14,7 +15,13 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fonstLoaded ? <Center flex={1} bg='$gray700'><Text>Home</Text></Center> : <View />}
+      {fonstLoaded ? (
+        <Center flex={1} bg='$gray700'>
+          <Text>Home</Text>
+        </Center>
+      ) : (
+        <Loading />
+      )}
 
     </GluestackUIStyledProvider>
   );
