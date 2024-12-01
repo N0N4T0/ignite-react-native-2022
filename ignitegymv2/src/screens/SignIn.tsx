6fd6d2475e8cc1483866a8575f2002d5085312de
Lúcio Nonato Oliveira <lucio.nonato@kivid.com.br>
2024-12-01 20:11:23 -0300

    fix: add variant on button

--- ignitegymv2/src/screens/SignIn.tsx
+++ ignitegymv2/src/screens/SignIn.tsx
@@ -31,6 +31,13 @@ export function SignIn() {
                     <Input placeholder="Senha" secureTextEntry />
                     <Button title='Acessar' />
                 </Center>
+
+                <Center flex={1} justifyContent="flex-end" marginTop="$4">
+                    <Text color="$gray100" fontSize="$sm" mb="$3" fontFamily="$body">
+                        Ainda não tem acesso?
+                    </Text>
+                    <Button title="Criar conta" variant="outline" />
+                </Center>
             </VStack>
         </VStack>
     )
