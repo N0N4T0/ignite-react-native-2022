08d224c0d909e463c09f8e0001dd2632d9e3fcc1
Lúcio Nonato Oliveira <lucio.nonato@kivid.com.br>
2024-12-03 19:08:27 -0300

    feat: add SignUp

--- ignitegymv2/App.tsx
+++ ignitegymv2/App.tsx
@@ -3,7 +3,7 @@ import { config } from './config/gluestack-ui.config';
 import { Center, GluestackUIStyledProvider, Text } from '@gluestack-ui/themed';
 import { StatusBar } from 'react-native';
 import { Loading } from '@components/Loading';
-import { SignIn } from '@screens/SignIn';
+import { SignUp } from '@screens/SignUp';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -16,7 +16,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <SignUp /> : <Loading />}
 
     </GluestackUIStyledProvider>
   );
