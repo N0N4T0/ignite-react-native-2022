fb0ba87e792836bd72719613e697929757c72494
Lúcio Nonato Oliveira <lucio.nonato@kivid.com.br>
2024-12-08 13:46:43 -0300

    fix: add logout button on Header

--- ignitegymv2/src/components/HomeHeader.tsx
+++ ignitegymv2/src/components/HomeHeader.tsx
@@ -1,4 +1,5 @@
-import { Heading, HStack, Text, VStack } from '@gluestack-ui/themed'
+import { Heading, HStack, Icon, Text, VStack } from '@gluestack-ui/themed'
+import { LogOut } from 'lucide-react-native'
 import { UserPhoto } from './UserPhoto'
 
 export function HomeHeader() {
@@ -6,7 +7,7 @@ export function HomeHeader() {
     <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap='$4'>
       <UserPhoto source={{ uri: 'https://github.com/N0N4T0.png'}} w='$16' h='$16' alt='Imagem do usuário' />
       
-      <VStack>
+      <VStack flex={1}>
         <Text color="$gray100" fontSize="$sm">
           Olá
         </Text>
@@ -14,6 +15,8 @@ export function HomeHeader() {
           Lúcio Nonato
         </Heading>
       </VStack>
+
+      <Icon as={LogOut} color='$gray200' size='xl' />
     </HStack>
   )
 }
