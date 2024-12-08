08892599bf5ae8674c4339c991497f2208ed426d
Lúcio Nonato Oliveira <lucio.nonato@kivid.com.br>
2024-12-07 23:43:11 -0300

    fix: add style on BottomNavigator

--- ignitegymv2/src/routes/app.routes.tsx
+++ ignitegymv2/src/routes/app.routes.tsx
@@ -8,6 +8,7 @@ import { Exercise } from '@screens/Exercise'
 import { History } from '@screens/History'
 import { Home } from '@screens/Home'
 import { Profile } from '@screens/Profile'
+import { Platform } from 'react-native'
 
 type AppRoutes = {
   home: undefined
@@ -30,6 +31,13 @@ export function AppRoutes() {
         tabBarShowLabel: false,
         tabBarActiveTintColor: tokens.colors.green500,
         tabBarInactiveTintColor: tokens.colors.gray200,
+        tabBarStyle: {
+          backgroundColor: tokens.colors.gray600,
+          borderTopWidth: 0,
+          height: Platform.OS === 'android' ? 'auto' : 96,
+          paddingBottom: tokens.space['10'],
+          paddingTop: tokens.space['6'],
+        },
       }}
     >
      <Screen
@@ -59,7 +67,16 @@ export function AppRoutes() {
           ),
         }}
       />
-      <Screen name="exercise" component={Exercise} />
+      <Screen
+        name="exercise"
+        component={Exercise}
+        options={{ 
+          tabBarButton: () => null,
+          tabBarItemStyle: {
+            display: 'none'
+          } 
+        }}
+      />
     </Navigator>
   )
 }
