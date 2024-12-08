c18774b7220feae1e0c5e0f6ec2306bdf5cab473
Lúcio Nonato Oliveira <lucio.nonato@kivid.com.br>
2024-12-08 13:12:30 -0300

    fix: add HomeHeader

---
+++ ignitegymv2/src/components/HomeHeader.tsx
@@ -0,0 +1,19 @@
+import { Heading, HStack, Text, VStack } from '@gluestack-ui/themed'
+import { UserPhoto } from './UserPhoto'
+
+export function HomeHeader() {
+  return (
+    <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap='$4'>
+      <UserPhoto source={{ uri: 'https://github.com/N0N4T0.png'}} w='$16' h='$16' alt='Imagem do usuário' />
+      
+      <VStack>
+        <Text color="$gray100" fontSize="$sm">
+          Olá
+        </Text>
+        <Heading color="$gray100" fontSize="$md">
+          Lúcio Nonato
+        </Heading>
+      </VStack>
+    </HStack>
+  )
+}
