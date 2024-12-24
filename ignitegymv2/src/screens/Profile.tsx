59c03d373d36be6b7c833f16d3b020684267bcc9
Lúcio Nonato Oliveira <lucio.nonato@kivid.com.br>
2024-12-24 08:16:14 -0300

    fix: add user picture

--- ignitegymv2/src/screens/Profile.tsx
+++ ignitegymv2/src/screens/Profile.tsx
@@ -1,11 +1,22 @@
 import { ScreenHeader } from "@components/ScreenHeader";
-import { VStack } from "@gluestack-ui/themed";
+import { UserPhoto } from "@components/UserPhoto";
+import { Center, ScrollView, VStack } from "@gluestack-ui/themed";
 
 
 export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
+
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt="$6" px="$10">
+          <UserPhoto
+            source={{ uri: 'https://github.com/N0N4T0.png' }}
+            size="xl"
+            alt="Imagem do usuário"
+          />
+        </Center>
+      </ScrollView>
     </VStack>
   )
 }
