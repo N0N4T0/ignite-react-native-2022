b71ed5866e127271f90d9679eb06eb6cd432d519
Lúcio Nonato Oliveira <lucio.nonato@kivid.com.br>
2024-12-24 07:27:43 -0300

    fix: create HistoryCard

--- ignitegymv2/src/screens/History.tsx
+++ ignitegymv2/src/screens/History.tsx
@@ -1,10 +1,12 @@
 import { ScreenHeader } from '@components/ScreenHeader'
 import { VStack } from '@gluestack-ui/themed'
+import { HistoryCard } from './HistoryCard'
 
 export function History() {
   return (
     <VStack flex={1}>
-    <ScreenHeader title="Histórico" />
-  </VStack>
+      <ScreenHeader title="Histórico" />
+      <HistoryCard/>
+    </VStack>
   )
 }
