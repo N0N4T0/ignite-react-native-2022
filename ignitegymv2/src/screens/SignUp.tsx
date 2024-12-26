6cc210e5a8533dd455fa2befa009395aa076f121
Lúcio Nonato Oliveira <lucio.nonato@kivid.com.br>
2024-12-26 11:04:41 -0300

    fix: get form data

--- ignitegymv2/src/screens/SignUp.tsx
+++ ignitegymv2/src/screens/SignUp.tsx
@@ -11,11 +11,20 @@ import {
     VStack,
 } from '@gluestack-ui/themed'
 import { useNavigation } from '@react-navigation/native'
+
+import { Controller, useForm } from 'react-hook-form'
+
 export function SignUp() {
+    const { control, handleSubmit } = useForm()
+
     const navigation = useNavigation()
     
     function handleGoBack() {
       navigation.goBack()
+    }
+
+    function handleSignUp(data: any) {
+        console.log(data)
     }
 
     return (
@@ -41,14 +50,62 @@ export function SignUp() {
                     </Center>
                     <Center flex={1} gap="$2">
                         <Heading color="$gray100">Crie sua conta</Heading>
-                        <Input placeholder="Nome" />
-                        <Input
-                            placeholder="E-mail"
-                            keyboardType="email-address"
-                            autoCapitalize="none"
+
+                        <Controller
+                            control={control}
+                            name="name"
+                            render={({ field: { onChange, value } }) => (
+                                <Input
+                                    placeholder="Nome"
+                                    onChangeText={onChange}
+                                    value={value}
+                                />
+                            )}
                         />
-                        <Input placeholder="Senha" secureTextEntry />
-                        <Button title="Criar e acessar" />
+
+                        <Controller
+                            control={control}
+                            name="email"
+                            render={({ field: { onChange, value } }) => (
+                                <Input
+                                    placeholder="E-mail"
+                                    keyboardType="email-address"
+                                    autoCapitalize="none"
+                                    onChangeText={onChange}
+                                    value={value}
+                                />
+                            )}
+                        />
+
+                        <Controller
+                            control={control}
+                            name="password"
+                            render={({ field: { onChange, value } }) => (
+                                <Input
+                                    placeholder="Senha"
+                                    secureTextEntry
+                                    onChangeText={onChange}
+                                    value={value}
+                                />
+                            )}
+                        />
+
+                         <Controller
+                            control={control}
+                            name="password_confirm"
+                            render={({ field: { onChange, value } }) => (
+                                <Input
+                                    placeholder="Confirme a Senha"
+                                    secureTextEntry
+                                    onChangeText={onChange}
+                                    value={value}
+                                    onSubmitEditing={handleSubmit(handleSignUp)}
+                                    returnKeyType="send"
+                                />
+                            )}
+                        />
+                                    
+                        <Button title="Criar e acessar" onPress={handleSubmit(handleSignUp)} />
                     </Center>
                     <Button title="Voltar para o login" variant="outline" mt="$12" onPress={handleGoBack}/>
                 </VStack>
