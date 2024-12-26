35a3d384ecb7fff6c3cc7c58f0cc3e94f63ba0eb
Lúcio Nonato Oliveira <lucio.nonato@kivid.com.br>
2024-12-26 11:55:47 -0300

    fix: add schema validation

--- ignitegymv2/src/screens/SignUp.tsx
+++ ignitegymv2/src/screens/SignUp.tsx
@@ -10,6 +10,10 @@ import {
     Text,
     VStack,
 } from '@gluestack-ui/themed'
+
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
 import { useNavigation } from '@react-navigation/native'
 
 import { Controller, useForm } from 'react-hook-form'
@@ -21,12 +25,27 @@ type FormDataProps = {
     password_confirm: string
   }
 
+const signUpSchema = yup.object({
+    name: yup.string().required('Informe o nome.'),
+    email: yup.string().required('Informe o e-mail.').email('E-mail inválido.'),
+    password: yup
+        .string()
+        .required('Informe a senha.')
+        .min(6, 'A senha deve ter pelo menos seis dígitos.'),
+    password_confirm: yup
+        .string()
+        .required('Confirme a senha')
+        .oneOf([yup.ref('password'), ''], 'A confirmação da senha não confere.'),
+})
+
 export function SignUp() {
     const {
         control,
         handleSubmit,
         formState: { errors },
-      } = useForm<FormDataProps>()
+    } = useForm<FormDataProps>({
+        resolver: yupResolver(signUpSchema),
+    })
 
     const navigation = useNavigation()
     
@@ -70,9 +89,6 @@ export function SignUp() {
                         <Controller
                             control={control}
                             name="name"
-                            rules={{
-                                required: 'Informe o nome',
-                            }}
                             render={({ field: { onChange, value } }) => (
                                 <Input
                                     placeholder="Nome"
@@ -86,13 +102,6 @@ export function SignUp() {
                         <Controller
                             control={control}
                             name="email"
-                            rules={{
-                                required: 'Informe o e-mail',
-                                pattern: {
-                                    value:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                                    message: 'E-mail inválido'
-                                }
-                            }}
                             render={({ field: { onChange, value } }) => (
                                 <Input
                                     placeholder="E-mail"
@@ -114,6 +123,7 @@ export function SignUp() {
                                     secureTextEntry
                                     onChangeText={onChange}
                                     value={value}
+                                    errorMessage={errors.password?.message}
                                 />
                             )}
                         />
@@ -129,6 +139,7 @@ export function SignUp() {
                                     value={value}
                                     onSubmitEditing={handleSubmit(handleSignUp)}
                                     returnKeyType="send"
+                                    errorMessage={errors.password_confirm?.message}
                                 />
                             )}
                         />
