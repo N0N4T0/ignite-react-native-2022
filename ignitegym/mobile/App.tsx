d54b1c3ff9302cc2ce3a3476d927ef4071462ab5
Lúcio Nonato Oliveira <lucio.nonato@kivid.com.br>
2024-12-27 22:07:33 -0300

    refactor v2

---
+++ ignitegym/mobile/App.tsx
@@ -0,0 +1,23 @@
+import { Loading } from '@components/Loading';
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+import { GluestackUIStyledProvider } from '@gluestack-ui/themed';
+import { Routes } from '@routes/index';
+import { StatusBar } from 'react-native';
+import { config } from './config/gluestack-ui.config';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
+  return (
+    <GluestackUIStyledProvider config={config}>
+
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Routes /> : <Loading />}
+
+    </GluestackUIStyledProvider>
+  );
+}
